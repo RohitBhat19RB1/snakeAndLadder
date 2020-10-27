90fc1ce9f5019341ea61d0587736c9453c9b8f40
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-10-28 00:00:02 +0530

    Game started with single player at Start position 0

---
+++ SnakeNLadderTest.java
@@ -0,0 +1,9 @@
+public class SnakeNLadderTest 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("   ***** Welcome To Snake And Ladder Game *****");
+   		System.out.println("Game has started with single player at start position 0");
+	}
+
+}
