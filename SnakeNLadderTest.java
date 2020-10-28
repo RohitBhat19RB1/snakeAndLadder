ba7a90bc613143a126e96e751c7a950a77b5400c
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-10-29 01:09:14 +0530

    Modified: noPlay,snake and ladder options added

--- SnakeNLadderTest.java
+++ SnakeNLadderTest.java
@@ -3,16 +3,19 @@ public class SnakeNLadderTest
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("   ***** Welcome To Snake And Ladder Game *****");
+				s.calculatePlayerValue(int player, int diceValue)
    		System.out.println("Game has started with single player at start position 0");
 		SnakeNLadder s = new SnakeNLadder();
 		int diceValue = s.rollDice();
-                System.out.println("Player has rolled the dice and the value he got is " + diceValue );
+		System.out.println("Player has rolled the dice and the value he got is " + diceValue );
+
 	}
 }
-class SnakeNLadder 
+class SnakeNLadder
 {
-		int rollDice() 
+
+
+		int rollDice()
 		{
                         int n = 0;
                         Random r = new Random();
@@ -20,4 +23,29 @@ class SnakeNLadder
                       return(n == 0 ? 1 : n);
                 }
 
+		 int calculatePlayerValue(int player, int diceValue)
+	{
+		int x = 0;
+		Random r = new Random();
+		x = r.nextInt(3);
+		switch (x)
+		{
+		case 0:
+			System.out.println("No play");
+			break;
+		case 1:
+			System.out.println("Ladder");
+			player = player + diceValue;
+			System.out.println(player);
+			System.out.println("rolling dice again");
+			player = player + diceValue;
+			System.out.println(player);
+			break;
+		default:
+			System.out.println("Snake");
+			player = player - diceValue;
+			break;
+		}
+
+	}
 }
